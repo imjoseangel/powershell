5eef16d283569c5743a3c68c082ab4a3a6911f66
imjoseangel <josea.munoz@gmail.com>
2017-09-19 18:35:21 +0200

    Adding Extra Info to Get-ProcessVM

--- get-processvm.ps1
+++ get-processvm.ps1
@@ -1,1 +1,6 @@
 Get-Process | Select-Object -Property Name, @{N="MB"; E= {$_.VM / 1gb -as [int]}} | Sort-Object -Property MB -Descending | Select-Object -First 10
+
+# Another way is:
+
+Update-TypeData -TypeName System.Diagnostics.Process -MemberType ScriptProperty -MemberName MB -Value {$this.VM / 1gb -as [int]}
+Get-Process | Select-Object -Property Name, MB | Sort-Object -Property MB -Descending | Select-Object -First 10
