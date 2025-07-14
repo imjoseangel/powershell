0028197f1979cbe8924c6c5a4ba892e409a5a26c
Jose Angel Munoz <5019972+imjoseangel@users.noreply.github.com>
2025-07-14 18:40:16 +0200

    Update Export_ADUser.ps1

--- Export_ADUser.ps1
+++ Export_ADUser.ps1
@@ -1,3 +1,3 @@
-$AdminCredentials = Get-Credential "AQUANIMA\joseangel.munoz"
+$AdminCredentials = Get-Credential "imjoseangel"
 $select = Get-ADUser  -Server 180.209.16.71 -Credential $AdminCredentials -filter "Country -eq 'ES'" -Properties GivenName,Surname,MobilePhone,OfficePhone
 $select | export-csv c:\spain.csv
